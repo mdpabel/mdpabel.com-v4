ff83a9bf4f4f0ebcfd63861a70744eb6f568334b
MD Pabel <mdpabel385@gmail.com>
2026-05-07 18:20:11 +0600

    updated 2 blacklist and malware services

---
+++ src/data/testimonials.ts
@@ -0,0 +1,25 @@
+export const TESTIMONIALS = [
+  {
+    quote:
+      "I'm very satisfied with MD Pabel service. He saved my site from hackers and removed all malware attacks. Highly Recommended.",
+    author: 'Hassan Infinkey',
+    role: 'eCommerce Owner',
+    rating: 5,
+    source: 'Google Review',
+  },
+  {
+    quote:
+      'My website was suffering from some redirect malware. MD was able to take care of the problem for a reasonable fee. For me, he was a lifesaver. I will certainly go to him first should something like that happen again.',
+    author: 'Kendall Miller',
+    role: 'Founder',
+    rating: 5,
+    source: 'Google Review',
+  },
+  {
+    quote: 'Thanks for giving me great support. You are very nice team.',
+    author: 'Usama Javed',
+    role: 'WordPress Agency',
+    rating: 5,
+    source: 'Facebook Review',
+  },
+];
